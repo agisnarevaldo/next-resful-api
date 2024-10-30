23a3556fb3cd57dccf8637379b742340dcb22db1
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-30 07:53:49 +0700

    feat: show Posts list

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import {useEffect, useState} from "react";
+import Link from "next/link";
 
 export default function Home() {
     const [posts, setPosts] = useState([]);
@@ -16,17 +17,49 @@ export default function Home() {
         fetchData();
     }, []);
     return (
-        <div>
-            <h1>Home</h1>
+        <main style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            padding: "0 20px",
+            gap: "20px",
+        }}>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+            }}>
+                <h1>Posts</h1>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <ul>
+                        {posts.map((post) => (
+                            <li key={post.id}>
+                                <h2>{post.title}</h2>
+                                <p>{post.content}</p>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
 
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <h2>{post.title}</h2>
-                        <p>{post.content}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+            <Link
+                href="/create"
+                style={{
+                    padding: "10px 20px",
+                    backgroundColor: "blue",
+                    color: "white",
+                    borderRadius: "5px",
+                    textDecoration: "none",
+                }}
+            >
+                Post Create
+            </Link>
+        </main>
     );
 }
