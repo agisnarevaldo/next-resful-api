7cb3773e7b5a956ccf94a2b1a5a740dfe95f3275
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-30 07:54:53 +0700

    refactor: change api POST to use form data

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,5 +1,5 @@
 import prisma from "../../../../prisma/client";
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 
 export async function GET(){
     // get all posts
@@ -19,26 +19,43 @@ export async function GET(){
 }
 
 export async function POST(request){
-    // get all request
-    const {title, content} = await request.json();
+    const formData = await request.formData();
 
-    // create data post
-    const post = await prisma.post.create({
-        data: {
-            title: title,
-            content: content,
-        }
-    });
+    // get title and content from form data
+    const title = formData.get("title");
+    const content = formData.get("content");
 
-    // return response JSON
-    return NextResponse.json(
-        {
-            success: true,
-            message: "Post Created Successfully",
-            data: post,
-        },
-        {
-            status: 201,
-        }
-    );
+    // create new post
+    try {
+        const post = await prisma.post.create({
+            data: {
+                title: title,
+                content: content,
+            },
+        });
+
+        // return response JSON
+        return NextResponse.json(
+            {
+                success: true,
+                message: "Post Created",
+                data: post,
+            },
+            {
+                status: 201,
+            }
+        );
+    } catch (error) {
+        // return response JSON
+        return NextResponse.json(
+            {
+                success: false,
+                message: error.message,
+                data: null,
+            },
+            {
+                status: 500,
+            }
+        );
+    }
 }
